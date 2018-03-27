fddaf557ad1dca3712b5fac460c3cdf661c7fbe2
Guilherme Tavares <zetavares.rib@gmail.com>
2018-03-27 20:56:10 -0300

    master

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import os
-import requests
 import json
 
 from sanic import Sanic
 from sanic.response import json
+from sanic.request import RequestParameters
+
+from client import GithubClient
 
 app = Sanic('app')
 
 @app.route("/", methods=["POST", "GET"])
-async def test(request):
+async def hello(request):
     return json({'hello': 'word'})
+
+
+@app.route("/pulls/<org>/<project>", methods=["POST", "GET"])
+async def pr_api(request, org, project):
+    return json(GithubClient().pulls(org, project))
 
 app.run(host='0.0.0.0', port=int(os.environ['PORT']))
---
+++ client.py
@@ -0,0 +1,29 @@
+import requests
+import os
+
+
+class GithubClient(object):
+    BASE_URL = 'https://api.github.com/'
+    HEADERS = {
+        'Accept': 'github.v3; format=json',
+        'content-type': 'application/json',
+        'Authorization': 'token {}'.format(os.environ['GITHUBKEY']),
+    }
+
+    def _get(self, url):
+        return requests.get(
+            url,
+            headers=self.HEADERS,
+        ).json()
+
+    def pulls(self, org, project):
+        url = '{}repos/{}/{}/pulls'.format(self.BASE_URL, org, project)
+        data = self._get(url)
+
+        for item in data:
+            statuses_url = item.get('statuses_url')
+            item.update({
+                'statuses': self._get(statuses_url),
+            })
+
+        return data
